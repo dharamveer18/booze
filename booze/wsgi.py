a58270a25e35d5af6c114ba8b3ab1cd920a4bc42
dharamveer18 <dharamveer@zyneto.in>
2026-09-18 11:17:20 +0530

    Refactor WSGI configuration to support local and PythonAnywhere paths, ensuring proper application loading based on the environment.

--- booze/wsgi.py
+++ booze/wsgi.py
@@ -8,9 +8,19 @@ https://docs.djangoproject.com/en/6.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+# Project root: localhost vs PythonAnywhere
+LOCAL_PATH = "/var/www/Dharamveer/booze"
+PYTHONANYWHERE_PATH = "/home/boozze/booze"
+
+path = PYTHONANYWHERE_PATH if os.path.isdir(PYTHONANYWHERE_PATH) else LOCAL_PATH
+
+if path not in sys.path:
+    sys.path.insert(0, path)
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "booze.settings")
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'booze.settings')
 
 application = get_wsgi_application()
